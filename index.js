4dbecf0df0d017b394ec36b4dfca0771d63aef13
Mason Hendrick <super40111@gmail.com>
2022-04-20 09:55:18 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-// Import the functions you need from the SDKs you need
-import initializeApp from "firebase/app";
-import doc, getDoc, getDocFromCache, getFirestore, setDoc, updateDoc from 'firebase/firestore'
-import getAnalytics from "firebase/analytics";
-import getAuth, createUserWithEmailAndPassword, onAuthStateChanged, updateProfile, sendPasswordResetEmail, signOut from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "firebase/app";
+import { doc, getDoc, getDocFromCache, getFirestore, setDoc, updateDoc } from 'firebase/firestore'
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged, updateProfile, sendPasswordResetEmail, signOut } from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = initializeApp({
   apiKey: "AIzaSyAfnxyM2o2uNrcunKE2ZocNep5HFc8PauU",
   authDomain: "gloss-d2259.firebaseapp.com",
