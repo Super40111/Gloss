93b0d76a6e30aa517031519522eab0353a327c3d
Mason Hendrick <super40111@gmail.com>
2022-04-20 09:46:18 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Import the functions you need from the SDKs you need
 import initializeApp from "firebase/app";
-import { doc, getDoc, getDocFromCache, getFirestore, setDoc, updateDoc } from 'firebase/firestore'
-import { getAnalytics } from "firebase/analytics";
-import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged, updateProfile, sendPasswordResetEmail, signOut } from "firebase/auth";
+import doc, getDoc, getDocFromCache, getFirestore, setDoc, updateDoc from 'firebase/firestore'
+import getAnalytics from "firebase/analytics";
+import getAuth, createUserWithEmailAndPassword, onAuthStateChanged, updateProfile, sendPasswordResetEmail, signOut from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
