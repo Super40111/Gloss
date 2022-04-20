b6176dfecad6837b3e8b22301b5da0384a2aa406
Mason Hendrick <super40111@gmail.com>
2022-04-20 12:45:21 -0700

    Update indexModules.js

--- indexModules.js
+++ indexModules.js
@@ -1,3 +1,7 @@
+import {test, CreateAccount, validPassword, displayAccountInfo, login, resetPassword, addStory, displayScores, updateScores } from 'index.js'
+
+
+
 function test() {
   alert("src Works");
 }
